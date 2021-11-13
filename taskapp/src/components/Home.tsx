e001f90f649efb2d477ede03748f360c36c52aae
Kevin Chan <kevinchan984@gmail.com>
2021-11-13 12:20:30 +0800

    routes

---
+++ taskapp/src/components/Home.tsx
@@ -0,0 +1,13 @@
+import{ ReactElement } from 'react'
+
+interface Props {
+    
+}
+
+export default function Home({}: Props): ReactElement {
+    return (
+        <div>
+            Hi
+        </div>
+    )
+}
