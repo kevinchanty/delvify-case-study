16e35186e771b329833ef58e6ea478f08b3d5d11
Kevin Chan <kevinchan984@gmail.com>
2021-11-13 22:00:58 +0800

    checkbox updated

--- taskapp/src/pages/HomePage.tsx
+++ taskapp/src/pages/HomePage.tsx
@@ -1,6 +1,8 @@
 import { ReactElement, useState } from 'react'
-import { Badge, Button, Form, FormControl, InputGroup, ListGroup, Modal } from 'react-bootstrap'
+import { Badge, Button, FormControl, ListGroup, Modal } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom';
+import { IoMenuOutline, IoCloseOutline } from "react-icons/io5";
+
 
 interface Props {
 
@@ -8,15 +10,14 @@ interface Props {
 
 export default function HomePage({ }: Props): ReactElement {
     const navigate = useNavigate();
-    
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+
+    const [showAdd, setShowAdd] = useState(false);
+    const [showDelete, setShowDelete] = useState(false);
+    const handleClose = () => setShowAdd(false);
+    const handleShow = () => setShowAdd(true);
 
     return (
         <>
-
-
             <div className="d-flex justify-content-between">
                 <h2>
                     My List
@@ -27,15 +28,14 @@ export default function HomePage({ }: Props): ReactElement {
                 <ListGroup.Item
                     as="li"
                     className="d-flex justify-content-between align-items-start"
-                    action
-                    onClick={() => navigate("/list")}
+                    action   
                 >
-                    <div className="ms-2 me-auto">
+                    <div className="ms-2 me-auto flex-fill" onClick={() => navigate("/list")}>
                         <div className="fw-bold">List Name </div>
                     </div>
-                    <Badge bg="primary" pill>
-                        14
-                    </Badge>
+                    <div className="fs-3" onClick={()=>setShowDelete(true)}>
+                        <IoCloseOutline />
+                    </div>
                 </ListGroup.Item>
                 <ListGroup.Item
                     as="li"
@@ -61,7 +61,7 @@ export default function HomePage({ }: Props): ReactElement {
                 </ListGroup.Item>
             </ListGroup>
 
-            <Modal show={show} onHide={handleClose}>
+            <Modal show={showAdd} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>Enter a Name:</Modal.Title>
                 </Modal.Header>
@@ -74,6 +74,20 @@ export default function HomePage({ }: Props): ReactElement {
                     </Button>
                     <Button variant="primary" onClick={handleClose}>
                         Add
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            <Modal show={showDelete} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirm to Delete LISTNAME ?</Modal.Title>
+                </Modal.Header>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={()=>setShowDelete(false)}>
+                        Close
+                    </Button>
+                    <Button variant="danger" onClick={handleClose}>
+                        Delete
                     </Button>
                 </Modal.Footer>
             </Modal>
--- taskapp/src/pages/ListPage.tsx
+++ taskapp/src/pages/ListPage.tsx
@@ -1,50 +1,107 @@
 import React, { ReactElement, useState } from 'react'
 import { Badge, Button, Form, FormControl, ListGroup, Modal } from 'react-bootstrap'
-import { IoMenuOutline, IoCloseOutline } from "react-icons/io5";
+import { IoMenuOutline, IoCloseOutline, IoAdd } from "react-icons/io5";
 
 
 interface Props {
 
 }
 
+const sampleData = [
+    {
+        id: 1,
+        name: "Task 1",
+        description: "sample description 1",
+        deadline: new Date(),
+        selected: false,
+    },
+    {
+        id: 2,
+        name: "Task 2",
+        description: "sample description 2",
+        deadline: new Date(),
+        selected: false,
+    },
+]
+
+const sampleData2 = [
+    {
+        id: 1,
+        name: "Task 1",
+        description: "sample description 3",
+        deadline: new Date(),
+        selected: false,
+    },
+    {
+        id: 2,
+        name: "Task 2",
+        description: "sample description 4",
+        deadline: new Date(),
+        selected: false,
+    },
+]
 function alertClicked() {
     alert('You clicked the third ListGroupItem');
 }
 
 export default function ListPage({ }: Props): ReactElement {
+    const [taskList, setTaskList] = useState(sampleData)
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    function mySetSelected(index: number, checked: boolean) {
+        console.log(checked)
+        setTaskList(prevTaskList => {
+            let newState = [...prevTaskList]
+            newState[index].selected = checked
+            return newState
+        })
+        console.log(taskList)
+    }
 
     return (
         <>
             <div className="d-flex justify-content-between">
                 <h2>LIST NAME</h2>
-                <Button variant="primary" size="sm" onClick={handleShow}>Add Task</Button>
+                <div onClick={handleShow} className="fs-3">
+                    <IoAdd />
+                </div>
             </div>
-            <ListGroup as="ol" numbered>
-                <ListGroup.Item
-                    as="li"
-                    className="d-flex justify-content-between align-items-start"
-                // action onClick={alertClicked}
-                >
-                    <div className="ms-2 me-auto">
-                        <div className="d-flex">
-                            <div className="fw-bold me-3">Task 1</div>
-                            <Badge bg="primary" pill >
-                                {new Date().toDateString()}
-                            </Badge>
+            <ListGroup as="ol">
+                {taskList.map((task, index) => (
+                    <ListGroup.Item
+                        key={index}
+                        as="li"
+                        className="d-flex justify-content-between align-items-start"
+                    // action onClick={alertClicked}
+                    >
+                        <Form.Check
+                            as="input"
+                            type="checkbox"
+                            id={`default-checkbox`}
+                            checked={task.selected}
+                            onChange={()=>mySetSelected(index, !task.selected)}
+                        />
+                        <div className="ms-2 me-auto">
+                            <div className="d-flex">
+                                <div className="fw-bold me-3">{task.name}</div>
+                                <Badge bg="primary" pill >
+                                    {task.deadline.toDateString()}
+                                </Badge>
+                            </div>
+                            <div>
+                                {task.description}
+                            </div>
                         </div>
-                        <div>
-                            Description
+                        <div className="fs-3">
+                            <IoMenuOutline />
+                            <IoCloseOutline />
                         </div>
-                    </div>
-                    <div className="fs-3">
-                        <IoMenuOutline />
-                        <IoCloseOutline />
-                    </div>
 
-                </ListGroup.Item>
+                    </ListGroup.Item>
+                ))
+                }
             </ListGroup>
 
 
